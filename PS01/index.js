5d16f28d3131a5c1fe229147fee19142e0526ae1
ZiyiZhao93 <31828737+ZiyiZhao93@users.noreply.github.com>
2017-11-07 01:02:16 -0500

    11.7

--- PS01/index.js
+++ PS01/index.js
@@ -4,13 +4,17 @@ var height = document.getElementById('svg1').clientHeight;
 var marginLeft = 0;
 var marginTop = 0;
 
-var svg = d3.select('svg')
+var svg = d3.select('#svg1')
+    .append('g')
+    .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
+
+var svg2 = d3.select('#svg2')
     .append('g')
     .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
 //set up the projection for the map
 var albersProjection = d3.geoAlbers()  //tell it which projection to use
-    .scale(100000)//tell it how big the map should be
+    .scale(90000)//tell it how big the map should be
     .rotate( [71.057,0] )
     .center( [0, 42.313] ) //FIND CENTER POINT LAT/LONG VALUE OF MASSACHUSETTS
     .translate([(width/2), (height/2)]);  //set the center of the map to show up in the center of the screen
@@ -19,9 +23,38 @@ var albersProjection = d3.geoAlbers()  //tell it which projection to use
 path = d3.geoPath()
     .projection(albersProjection);        //tell it to use the projection that we just made to convert lat/long to pixels
 
-var stateLookup = d3.map();
+var cityLookup = d3.map();
 
-var colorScale = d3.scaleLinear().range(['white','pink']);
+var colorScale = d3.scaleLinear().range(['white','blue']);
+
+var scaleX = d3.scaleBand().rangeRound([0, width-2*marginLeft]).padding(0.1);
+var scaleY = d3.scaleLinear().range([height-2*marginTop, 0]);
+
+/*
+subway line
+queue()
+    .defer(d3.json, "./subway.json")
+    .defer(d3.csv, "./subway.csv")
+    .await(function(err, mapData, populationData) {
+
+
+        populationData.forEach(function (d) {
+            cityLookup.set(d.name, d.population);
+        });
+
+
+        svg.selectAll("path")               //make empty selection
+            .data(mapData.features)          //bind to the features array in the map data
+            .enter()
+            .append("path")                 //add the paths to the DOM
+            .attr("d", path)                //actually draw them
+            .attr("class", "feature")
+            .attr('stroke', 'white')
+            .attr('stroke-width', .2);
+
+    });
+*/
+
 
 queue()
     .defer(d3.json, "./boston1.json")
@@ -30,7 +63,7 @@ queue()
 
 
         populationData.forEach(function(d){
-            stateLookup.set(d.name, d.population);
+            cityLookup.set(d.name, d.population);
         });
 
 
@@ -43,12 +76,109 @@ queue()
             .attr("d", path)                //actually draw them
             .attr("class", "feature")
             .attr('fill',function(d){
-                return colorScale(stateLookup.get(d.properties.NAME));
+                return colorScale(cityLookup.get(d.properties.NAME));
             })
             .attr('stroke','white')
-            .attr('stroke-width',.2);
+            .attr('stroke-width',.2)
+            .on('mouseover',function (d) {
+                d3.select(this).attr('fill','pink');
+            })
+            .on('mouseout', function (d) {
+                d3.select(this).attr('fill', function (d) {
+                return colorScale(cityLookup.get(d.properties.NAME));
+            })
+        })
 
     });
 
+
+d3.csv('./subway.csv', function(dataIn) {
+
+    nestedData = d3.nest()
+        .key(function(d){return d.gender})
+        .entries(dataIn);
+
+    var loadData = nestedData.filter(function(d){return d.key == 'total'})[0].values;
+
+    svg.append("g")
+        .attr('class','xaxis')
+        .attr('transform','translate(0,'+ (height-2*marginTop) +')')
+        .call(d3.axisBottom(scaleX));
+
+    svg.append("g")
+        .attr('class', 'yaxis')
+        .call(d3.axisLeft(scaleY));
+
+    drawPoints(loadData);
+
+});
+
+function drawPoints(pointData){
+
+
+    scaleX.domain(pointData.map(function(d){return d.things;}));
+    scaleY.domain([0, d3.max(pointData.map(function(d){return +d.number}))]);
+
+
+    d3.selectAll('.xaxis')
+        .call(d3.axisBottom(scaleX));
+
+    d3.selectAll('.yaxis')
+        .call(d3.axisLeft(scaleY));
+
+
+    var rects = svg.selectAll('.bars')
+        .data(pointData, function(d){return d.things;});
+
+    console.log(pointData);
+
+    rects
+        .transition()
+        .duration(100)
+        .attr('x',function(d){
+            return scaleX(d.things);
+        })
+        .attr('y',function(d){
+            return scaleY(+d.number);
+        })
+        .attr('width',function(d){
+            return scaleX.bandwidth();
+        })
+        .attr('height',function(d){
+            return height-2*marginTop - scaleY(+d.number);
+        });
+
+    rects
+        .enter()
+        .append('rect')
+        .attr('class','bars')
+        .attr('fill', "mediumslateblue")
+        .attr('id',function (d) {return d.things})
+        .attr('x',function(d){
+            return scaleX(d.things);
+        })
+        .attr('y',function(d){
+            return scaleY(+d.number);
+        })
+        .attr('width',function(d){
+            return scaleX.bandwidth();
+        })
+        .attr('height',function(d){
+            return height-2*marginTop - scaleY(+d.number);
+        })
+
+}
+
+function updateData(selectedGender){
+    return nestedData.filter(function(d){return d.key == 'female'})[0].values;
+}
+
+
+function sliderMoved(value){
+
+    newData = updateData(value);
+    drawPoints(newData);
+
+}
 
 
